f691797974229278a6d00e84cbdd229b645f8092
Soumya <a2soumya.ranjan@gmail.com>
2022-04-30 14:59:59 +0530

    done

---
+++ react-redux/src/Redux/action.js
@@ -0,0 +1,13 @@
+
+export const ADD_TODO = "ADD_TODO"
+export const TOGGLE = "TOGGLE"
+export const addTodo = (data) => {
+  return {
+    type: ADD_TODO,
+    payload: data,
+  };
+};
+export const tog = {
+  type: TOGGLE,
+  payload : true
+}
---
+++ react-redux/src/Redux/store.js
@@ -0,0 +1,7 @@
+import {legacy_createStore as createStore} from "redux"
+import {counterReducer} from "./reducer";
+
+export const store = createStore(counterReducer,{todos:[]})
+
+
+
